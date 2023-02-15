d6f65d4fb60cab9391b4877cadbfba90dea9f671
yoonsun lee <yoonsunlee150@gmail.com>
2023-02-15 10:42:10 -0500

    threejswebflowdemo

--- src/main.js
+++ src/main.js
@@ -1,3 +1,30 @@
 import './styles/style.css'
 
-console.log('Hello')
+
+
+// Three
+import * as THREE from 'three';
+
+const canvas = document.querySelector('canvas.webgl')
+
+//Scene
+const scene = new THREE.Scene();
+
+
+//Camera
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.z=5
+scene.add(camera)
+
+//Geometry
+
+
+
+
+//render
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.render(scene, camera)
+
+
+document.body.appendChild( renderer.domElement );
